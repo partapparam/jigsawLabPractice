e2db2254502b46dbfe76330e69c1f39f7a4a1a01
partapparam <paramsingh1961@gmail.com>
2023-11-21 16:00:30 -0800

    tests

--- oop/testing_objects/school-objects/query_schools.py
+++ oop/testing_objects/school-objects/query_schools.py
@@ -1,22 +1,32 @@
 import pandas as pd
+from school import *
 
 def query_api():
     url = "https://raw.githubusercontent.com/jigsawlabs-student/decision-trees-intro/master/3-practical-ds-4/nyc_hs_sat.csv"
     df = pd.read_csv(url)
     return df.to_dict('records')
 
+def get_school(school, keys):
+    return {k:v for k, v in school.items() if k in keys}
 
 def build_instances(school_dicts):
     selected_attrs = ['dbn', 'name', 'boro', 'writing_score', 'math_avg', 
         'reading_avg', 'total_students', 'attendance_rate']
-    edited_schools = [{}]
+    selected_schools = [get_school(school, selected_attrs) for school in school_dicts]
+    schools = [School(**selected_school) for selected_school in selected_schools]
+    return schools
 
 
 def largest_total_score_school(schools):
-    pass
+    # return sorted(schools, key=lambda school: school.math_avg + school.reading_avg + school.writing_score)[-1]
+
+    # return sorted(schools, key=lambda school: school.total_score())[-1]
+
+    # we can also do this with max
+    return max(schools, key=lambda school: school.total_score())
 
 def average_total_score_of(schools):
-    pass
+    
 
 def largest_average_student_attendance(schools):
     pass
