9ab530a82c7dfa9047a19a3c8ff2daa73aa1b69c
partapparam <paramsingh1961@gmail.com>
2023-12-06 11:56:38 -0800

    practice

---
+++ oop/objects-practice/console.py
@@ -0,0 +1,7 @@
+from models.person import Person
+
+param = Person('param', 25)
+
+param.__dict__
+# __doc__ will print out the docstrings
+param.__doc__
---
+++ oop/objects-practice/models/person.py
@@ -0,0 +1,7 @@
+class Person:
+    """These are the docstrings for Person
+        A person should have name and age
+    """
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
