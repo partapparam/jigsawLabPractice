e058e9351691afb2e53508968d6e52ff6f736c2f
partapparam <paramsingh1961@gmail.com>
2023-12-20 18:27:42 -0800

    db save function

--- week8/school-lab-flask/api/lib/db.py
+++ week8/school-lab-flask/api/lib/db.py
@@ -10,15 +10,44 @@
 #drop record
 # get db
 
-from settings import TEST_DB_NAME, TEST_DB_USER
+from settings import TEST_DB_NAME, TEST_DB_USER, DB_NAME, DB_USER
 from flask import g
+from flask import current_app
 import psycopg2
 
 test_conn = psycopg2(TEST_DB_NAME, TEST_DB_USER, 'password')
 test_cursor = test_conn.cursor()
 
+conn = psycopg2(DB_NAME, DB_USER, 'password')
+
 def get_db(database, user):
-    if not g.db:
-        g.db = psycopg2(database, user, 'password')
+    # check to see if db exists in the 'g' context
+    if 'db' not in g:
+        # if not, we create it with the current app, the requesting app's context
+        g.db = psycopg2(dbname=current_app.config['DB_NAME'], user = current_app.config['DB_USER'], password = current_app['PASSWORD'])
     return g.db 
+
+def save(obj, conn):
+    # save query requires: table, column names and values
+    # obj.
+    table_name = obj.__table__
+    columns = keys(obj.__dict__)
+    values = values(obj.__dict__)
+    query = """INSERT INTO %s (%s) VALUES (%s)"""
+    cursor = conn.cursor()
+    cursor.execute(query, (table_name, columns, values,))
+    conn.commit()
+    get_query = """SELECT * FROM %s ORDER BY id LIMIT 1"""
+    cursor.execute(get_query, (table_name,))
+    saved = cursor.fetchone()
+    return saved
+
+def keys(obj):
+    key_list = obj.keys()
+    return ', '.join(key_list)
+
+def values(obj):
+    value_list = obj.values()
+    return ', '.join(value_list)
+
 
--- week8/school-lab-flask/app.py
+++ week8/school-lab-flask/app.py
@@ -1,7 +1,7 @@
 import api as api
-from settings import DB_NAME, DB_USER
+from settings import DB_NAME, DB_USER, PASSWORD
 
-app = api.create_app(DB_NAME, DB_USER, 'password')
+app = api.create_app(DB_NAME, DB_USER, PASSWORD)
 
 if __name__ == '__main__':
     app.run(debug=True)
