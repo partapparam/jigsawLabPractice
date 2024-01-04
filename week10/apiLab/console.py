1b3a8785cc890ca1826c9c2649f1df1698966c11
partapparam <paramsingh1961@gmail.com>
2024-01-03 18:19:24 -0800

    add new books

--- week10/apiLab/console.py
+++ week10/apiLab/console.py
@@ -8,12 +8,20 @@ from models.author_adapter import AuthorAdapter
 app = Flask(__name__)
 
 @app.route('/author/<author>')
-def artist(author):
+def author(author):
     client = ApiClient(URL=f'https://openlibrary.org/search/authors.json?q={author}')
     response = client.run()
     authors = AuthorAdapter().run(response['docs'])
     author_dicts = [author.__dict__ for author in authors] 
     return author_dicts
 
-
-
+@app.route('/author/works/<author>')
+def works(author):
+    client = ApiClient(URL=f'https://openlibrary.org/search.json?q={author}&sort=new')
+    response = client.run()
+    authors = AuthorAdapter().run(response['docs'])
+    author_dicts = [author.__dict__ for author in authors] 
+    return author_dicts
+search = 'JK Rowling' 
+client = ApiClient(URL=f'https://openlibrary.org/search.json?q={search}&sort=new')
+response = client.run()
