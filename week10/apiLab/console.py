4f43cace4bf179494c222f80ab088c1b3702423c
partapparam <paramsingh1961@gmail.com>
2024-01-03 18:25:21 -0800

    book model adapter pattern creation

--- week10/apiLab/console.py
+++ week10/apiLab/console.py
@@ -4,6 +4,7 @@
 from flask import Flask 
 from models.client import ApiClient
 from models.author_adapter import AuthorAdapter
+from models.book_adapter import BookAdapter
 
 app = Flask(__name__)
 
@@ -19,9 +20,6 @@ def author(author):
 def works(author):
     client = ApiClient(URL=f'https://openlibrary.org/search.json?q={author}&sort=new')
     response = client.run()
-    authors = AuthorAdapter().run(response['docs'])
-    author_dicts = [author.__dict__ for author in authors] 
-    return author_dicts
-search = 'JK Rowling' 
-client = ApiClient(URL=f'https://openlibrary.org/search.json?q={search}&sort=new')
-response = client.run()
+    books = BookAdapter().run(response['docs'])
+    book_dicts = [book.__dict__ for book in books] 
+    return book_dicts
